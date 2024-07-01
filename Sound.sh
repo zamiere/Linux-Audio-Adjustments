97d059412177d649bc39a5f9697aef020b630137
zamiere <zamiere@gmail.com>
2024-07-01 17:28:16 +0200

    Update Sound.sh

--- Sound.sh
+++ Sound.sh
@@ -8,19 +8,16 @@ chrt -f -p 54 $(pgrep ksoftirqd/2)
 chrt -f -p 54 $(pgrep ksoftirqd/3)
 
 #Uncomment for MPD Affinity and Priority
-#chrt -f -p 81 $(pidof mpd)
-#taskset -c -p 1 $(pidof mpd)
+chrt -f -p 81 $(pidof mpd)
+taskset -c -p 1 $(pidof mpd)
 
 #USB Dacs Uncomment to reduce USB latency
-#modprobe snd-usb-audio nrpacks=1
+modprobe snd-usb-audio nrpacks=1
 
 #SPDIF HAT and WiFi users Uncomment to turn off power to [Ethernet and USB] ports
 #echo 0x0 > /sys/devices/platform/soc/3f980000.usb/buspower
 
 #Reduce operating system latency
-echo noop > /sys/block/mmcblk0/queue/scheduler
-echo 1000000 > /proc/sys/kernel/sched_latency_ns
-echo 100000 > /proc/sys/kernel/sched_min_granularity_ns
-echo 25000 > /proc/sys/kernel/sched_wakeup_granularity_ns
+echo mq-deadline > /sys/block/mmcblk0/queue/scheduler
 
 exit
